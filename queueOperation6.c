2d6284023fb462e9075c21e224a137e8aa421568
RAYHAN HUSSAIN <135307421+rayhanhcse@users.noreply.github.com>
2023-06-11 21:19:39 +0600

    Update queueOperation6.c

--- queueOperation6.c
+++ queueOperation6.c
@@ -1,92 +1,121 @@
 #include<stdio.h>
-int stack[100],choice,n,top,x,i;
-void push(void);
-void pop(void);
-void display(void);
-int main( )
+# define MAX 20
+int cqueue_arr[MAX];
+int front = -1;
+int rear = -1;
+void enqueue(int item)
 {
-top=-1;
-printf("Enter the STACK size (maximum 100):");
-scanf("%d",&n);
-printf("\nEnter what operations you want to perform :");
-printf("\n\t 1.PUSH\n\t 2.POP\n\t 3.DISPLAY\n\t 4.EXIT\n");
+if((front == 0 && rear == MAX-1) || (front == rear+1))
+{
+printf("Queue is FULL! \n");
+return;
+}
+if(front == -1)
+{
+front = 0;
+rear = 0;
+}
+else
+{
+if(rear == MAX-1)
+rear = 0;
+else
+rear = rear+1;
+}
+cqueue_arr[rear] = item ;
+}
+
+
+
+void dequeue()
+{
+if(front == -1)
+{
+printf("Queue is EMPTY! \n");
+return ;
+}
+printf("Dequeued element is : %d\n",cqueue_arr[front]);
+if(front == rear)
+{
+front = -1;
+rear=-1;
+}
+else
+{
+if(front == MAX-1)
+front = 0;
+else
+front = front+1;
+}
+}
+void display()
+{
+int front_pos = front,rear_pos = rear;
+if(front == -1)
+{
+printf("Queue is EMPTY!\n");
+return;
+}
+printf("Queue elements :\n");
+if( front_pos <= rear_pos )
+
+
+
+while(front_pos <= rear_pos)
+{
+printf("%d ",cqueue_arr[front_pos]);
+front_pos++;
+}
+else
+{
+while(front_pos <= MAX-1)
+{
+printf("%d ",cqueue_arr[front_pos]);
+front_pos++;
+}
+front_pos = 0;
+while(front_pos <= rear_pos)
+{
+printf("%d ",cqueue_arr[front_pos]);
+front_pos++;
+}
+}
+printf("\n");
+}
+int main()
+{
+int choice,item;
+printf("Choose the operation you want to be performed : \n");
+printf("\t1.Enqueue\n");
+printf("\t2.Dequeue\n");
+printf("\t3.Display\n");
+printf("\t4.Quit\n");
 do
 {
-printf("\nEnter the Choice: ");
+
+
+
+printf("\nEnter your choice : ");
 scanf("%d",&choice);
 switch(choice)
 {
-case 1:
-{
-push( );
+case 1 :
+printf("Enqueue the element : ");
+scanf("%d", &item);
+enqueue(item);
 break;
-}
-case 2:
-{
-pop( );
-
-
-
+case 2 :
+dequeue();
 break;
-}
 case 3:
-{
-display( );
+display();
 break;
-}
 case 4:
-{
-printf("\n\t EXIT POINT ");
 break;
-}
 default:
-{
-printf ("\n\tPlease Enter a Valid Choice between 1/2/3/4");
-}
+printf("Choose a valid option between 1/2/3/4 \n");
 }
 }
 while(choice!=4);
 return 0;
-}
-void push( )
-{
-if(top>=n-1)
-{
-printf("\n\tSTACK is FULL!");
-}
-else
-{
-printf("Enter a value you want to push in stack: ");
-scanf("%d",&x);
-
-
-
-top++;
-stack[top]=x;
-}
-}
-void pop( )
-{
-if(top<=-1)
-{
-printf("\n\t Stack is EMPTY!");
-}
-else
-{
-printf("\nThe popped element is %d",stack[top]);
-top--;
-}
-}
-void display( )
-{
-if(top>=0)
-{
-printf("\nThe elements in STACK are :\n");
-for(i=top; i>=0; i--)
-printf("\n%d",stack[i]);
-}
-else
-{
-printf("\nThe STACK is EMPTY!");
-}
 }
